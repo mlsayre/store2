8ee164451cd8c8fa8edf3f8706cb68593c3019c4
Matt Sayre <mlsayre@gmail.com>
2013-12-08 20:26:17 -0800

    paperclip

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,12 @@
 class Product < ActiveRecord::Base
-  attr_accessible :name, :price, :released_on
+  attr_accessible :name, :price, :released_on, :avatar
   def to_param
     "#{id}-#{name.parameterize}"
   end
+
+  has_attached_file :avatar, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131013004404) do
+ActiveRecord::Schema.define(:version => 20131209041338) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
     t.decimal  "price"
     t.date     "released_on"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
